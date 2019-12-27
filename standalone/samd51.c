a594f7fa39d84b10b9974fbc4b6f67b362749214
Michel Megens <michel@michelmegens.net>
2019-12-27 13:39:09 +0100

    samd51: add standalone defs
    Add memory allocation defs.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- standalone/samd51.c
+++ standalone/samd51.c
@@ -5,6 +5,8 @@
  * @email  michel@michelmegens.net
  */
 
+#include <stdlib.h>
+#include <string.h>
 #include <sam.h>
 #include <core_cm4.h>
 
@@ -21,6 +23,56 @@ void lwiot_sleep(int ms)
 {
 	uint32_t next = __tick + (unsigned int)ms;
 	while(__tick < next) { }
+}
+
+void lwiot_udelay(uint32_t us)
+{
+	uint32_t start, elapsed;
+	uint32_t count;
+
+	if (us == 0)
+		return;
+
+	count = us * (F_CPU / 1000000) - 20;  // convert us to cycles.
+	start = DWT->CYCCNT;  //CYCCNT is 32bits, takes 37s or so to wrap.
+	while (1) {
+		elapsed = DWT->CYCCNT - start;
+		if (elapsed >= count)
+			return;
+	}
+}
+
+void enter_critical()
+{
+	__disable_irq();
+}
+
+void exit_critical()
+{
+	__enable_irq();
+}
+
+void lwiot_mem_free(void *ptr)
+{
+	free(ptr);
+}
+
+void *lwiot_mem_alloc(size_t size)
+{
+	return malloc(size);
+}
+
+void *lwiot_mem_zalloc(size_t size)
+{
+	void *ptr = malloc(size);
+
+	memset(ptr, 0, size);
+	return ptr;
+}
+
+void *lwiot_mem_realloc(void *ptr, size_t size)
+{
+	return realloc(ptr, size);
 }
 
 void no_os_init()
