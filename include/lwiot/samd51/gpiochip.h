eb16ccad1312d3a907606ea524783d10582190b0
Michel Megens <michel@michelmegens.net>
2020-01-02 17:23:20 +0100

    gpio: update the GPIO definitions
    Take the external GPIO definitions out of C++ scope.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- include/lwiot/samd51/gpiochip.h
+++ include/lwiot/samd51/gpiochip.h
@@ -8,10 +8,40 @@
 #pragma once
 
 #include <lwiot/lwiot.h>
+
+typedef enum {
+	EXTERNAL_INT_0 = 0,
+	EXTERNAL_INT_1,
+	EXTERNAL_INT_2,
+	EXTERNAL_INT_3,
+	EXTERNAL_INT_4,
+	EXTERNAL_INT_5,
+	EXTERNAL_INT_6,
+	EXTERNAL_INT_7,
+	EXTERNAL_INT_8,
+	EXTERNAL_INT_9,
+	EXTERNAL_INT_10,
+	EXTERNAL_INT_11,
+	EXTERNAL_INT_12,
+	EXTERNAL_INT_13,
+	EXTERNAL_INT_14,
+	EXTERNAL_INT_15,
+	EXTERNAL_INT_NMI,
+	EXTERNAL_NUM_INTERRUPTS,
+	NOT_AN_INTERRUPT = -1,
+	EXTERNAL_INT_NONE = NOT_AN_INTERRUPT,
+} ExtIrqs;
+
+#ifdef __cplusplus
+extern "C" void invokeIrq(ExtIrqs num);
+#else
+extern void invokeIrq(ExtIrqs num);
+#endif
+
+#ifdef __cplusplus
 #include <lwiot/io/gpiochip.h>
 #include <lwiot/stl/referencewrapper.h>
 
-#ifdef __cplusplus
 namespace lwiot
 {
 	namespace samd51
@@ -22,29 +52,7 @@ namespace lwiot
 
 		namespace detail_gpio
 		{
-			typedef enum
-			{
-				EXTERNAL_INT_0 = 0,
-				EXTERNAL_INT_1,
-				EXTERNAL_INT_2,
-				EXTERNAL_INT_3,
-				EXTERNAL_INT_4,
-				EXTERNAL_INT_5,
-				EXTERNAL_INT_6,
-				EXTERNAL_INT_7,
-				EXTERNAL_INT_8,
-				EXTERNAL_INT_9,
-				EXTERNAL_INT_10,
-				EXTERNAL_INT_11,
-				EXTERNAL_INT_12,
-				EXTERNAL_INT_13,
-				EXTERNAL_INT_14,
-				EXTERNAL_INT_15,
-				EXTERNAL_INT_NMI,
-				EXTERNAL_NUM_INTERRUPTS,
-				NOT_AN_INTERRUPT = -1,
-				EXTERNAL_INT_NONE = NOT_AN_INTERRUPT,
-			} ExtIrqs ;
+			typedef ::ExtIrqs ExtIrqs;
 
 			typedef enum {
 				PIO_NOT_A_PIN=-1,
@@ -96,6 +104,7 @@ namespace lwiot
 			void odWrite(int pin, bool value) override;
 			void attachIrqHandler(int pin, irq_handler_t handler, IrqEdge edge) override;
 			void detachIrqHandler(int pin) override;
+			void invoke(ExtIrqs num);
 
 #ifdef HAVE_DEBUG
 			void printPinMappings();
@@ -107,7 +116,9 @@ namespace lwiot
 			void init();
 			void initIrqs();
 
-			detail_gpio::IrqMap irq_map_[detail_gpio::EXTERNAL_NUM_INTERRUPTS];
+			detail_gpio::IrqMap irq_map_[EXTERNAL_NUM_INTERRUPTS];
+			bool initialized_;
+			static constexpr uint32_t NOT_INITIALIZED = 0xFFFF;
 		};
 	}
 }
