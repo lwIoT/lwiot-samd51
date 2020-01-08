b5541b14970fb8d9a5801910cf8e8c9efb519ef6
Michel Megens <michel@michelmegens.net>
2020-01-08 20:04:27 +0100

    tests: update the init test
    Implement PWM.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- tests/startup/app.cpp
+++ tests/startup/app.cpp
@@ -9,10 +9,13 @@
 
 #include <lwiot.h>
 #include <sam.h>
+#include <hal_gpio.h>
 
 #include <lwiot/samd51/gpio.h>
 #include <lwiot/samd51/gpiochip.h>
 #include <lwiot/samd51/hardwarei2calgorithm.h>
+#include <lwiot/samd51/adcchip.h>
+#include <lwiot/samd51/pwm.h>
 
 volatile int irq_triggered;
 
@@ -28,11 +31,24 @@ extern "C" void app_main()
 	lwiot::GpioPin scl(GPIO(GPIO_PORTB, 3));
 	lwiot::GpioPin led(GPIO(GPIO_PORTA, 16));
 	lwiot::GpioPin irq_pin12(GPIO(GPIO_PORTA, 17));
+	lwiot::GpioPin pwm_1(GPIO(GPIO_PORTB, 15));
+
 	auto chip = lwiot::samd51::GpioChip::Instance();
 
 	lwiot::samd51::gpio_mux_pin(sda, PINMUX_PB02D_SERCOM5_PAD0);
 	lwiot::samd51::gpio_mux_pin(scl, PINMUX_PB03D_SERCOM5_PAD1);
+
 	lwiot::samd51::HardwareI2CAlgorithm algo(scl, sda, 100000, SERCOM5);
+	lwiot::samd51::AdcChip adcdev;
+
+	lwiot::samd51::PwmTimer pwm(lwiot::samd51::PWM::PWM_0, 2000, lwiot::samd51::PWM0_CHANNELS);
+
+	pin_set_peripheral(pwm_1.pin(), EPioType::PIO_PWM_G);
+	pwm[3].setDutyCycle(0.5);
+	pwm[3].setGpioPin(pwm_1);
+	pwm[3].enable();
+
+	adcdev.begin();
 
 	led.output();
 	irq_pin12.input();
@@ -43,6 +59,9 @@ extern "C" void app_main()
 	while (1) {
 		led.write(value);
 		print_dbg("IRQ triggered: %i\n", irq_triggered);
+		auto adcvalue = adc.read(2);
+		adcvalue = adc.toVoltage(adcvalue);
+		print_dbg("ADC value: %u\n", adcvalue);
 
 		value = !value;
 		lwiot_sleep(500);
