35384dce05846c4dd08ae733eb19b16633d4a064
Michel Megens <michel@michelmegens.net>
2020-01-09 14:19:10 +0100

    tests: test the DAC
    Implement an integration test for the DAC.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- tests/startup/app.cpp
+++ tests/startup/app.cpp
@@ -15,6 +15,7 @@
 #include <lwiot/samd51/gpiochip.h>
 #include <lwiot/samd51/hardwarei2calgorithm.h>
 #include <lwiot/samd51/adcchip.h>
+#include <lwiot/samd51/dacchip.h>
 #include <lwiot/samd51/pwm.h>
 
 volatile int irq_triggered;
@@ -40,15 +41,18 @@ extern "C" void app_main()
 
 	lwiot::samd51::HardwareI2CAlgorithm algo(scl, sda, 100000, SERCOM5);
 	lwiot::samd51::AdcChip adcdev;
+	lwiot::samd51::DacChip dacdev;
 
 	lwiot::samd51::PwmTimer pwm(lwiot::samd51::PWM::PWM_0, 2000, lwiot::samd51::PWM0_CHANNELS);
-
 	pin_set_peripheral(pwm_1.pin(), EPioType::PIO_PWM_G);
-	pwm[3].setDutyCycle(0.5);
+	pwm[3].setDutyCycle(2.0/3.0);
 	pwm[3].setGpioPin(pwm_1);
 	pwm[3].enable();
 
 	adcdev.begin();
+	dacdev.begin();
+
+	dacdev.enable(0);
 
 	led.output();
 	irq_pin12.input();
@@ -59,6 +63,7 @@ extern "C" void app_main()
 	while (1) {
 		led.write(value);
 		print_dbg("IRQ triggered: %i\n", irq_triggered);
+		dacdev.write(0, 1400);
 		auto adcvalue = adc.read(2);
 		adcvalue = adc.toVoltage(adcvalue);
 		print_dbg("ADC value: %u\n", adcvalue);
